3fa4e8c40a1ac3788438d9415c730397cc58759f
Uwes Ahabab <uwesahabab@gmail.com>
2024-04-24 21:33:37 +0300

    pending

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [items, setItems] = useState(true);
+  const itemClass = items ? "" : "in-cart";
+  const color = items ? "add" : "remove";
+  function addBtnHandler() {
+    setItems(() => !items);
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={color} onClick={addBtnHandler}>
+        {items ? "Add to Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("All");
+  const handleChange = (event) => {
+    setFilterBy(event.target.value);
+  };
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
